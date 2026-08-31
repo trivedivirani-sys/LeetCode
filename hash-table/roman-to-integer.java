dcc12c0192ac5992ac50011b7e39b266a9398bf0
trivedivirani-sys <trivedivirani@gmail.com>
2026-08-31 17:25:20 +0530

    LC-roman-to-integer [Runtime: N/A; Memory: N/A]

---
+++ hash-table/roman-to-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int romanToInt(String s) {
+        int []values={
+            1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        
+        String []s={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    }
+    
+    for(int i=0;i<s.length;i++){
+        while(values<=s[i]){
+
+        }
+    }
+}
