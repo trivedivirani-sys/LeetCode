9c75ed7d7f16786155f9a0698039ff5713f46dbf
trivedivirani-sys <trivedivirani@gmail.com>
2026-09-01 20:23:35 +0530

    LC-longest-valid-parentheses [Runtime: 5 ms; Memory: 46.7 MB]

--- string/longest-valid-parentheses.java
+++ string/longest-valid-parentheses.java
@@ -5,7 +5,7 @@ class Solution {
         int ans=0;
     
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=="("){
+            if(s.charAt(i)=='('){
                 st.push(i);
             }
          else {
